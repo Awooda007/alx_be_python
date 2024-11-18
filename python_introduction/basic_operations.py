c478b8be8a2b388ad72e238e532dc9f85fd8e579
awooda007 <frankstyles9@gmail.com>
2024-11-18 14:38:19 +0000

    ekow: final code logic for addition, substraction and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1, number2 = 10,5
+
+print(f'Addition of 10 and 5 is {number1 + number2}')
+print(f'Subtraction of 10 and 5 is {number1 - number2}')
+print(f'Multiplication  of 10 and 5 is {number1 * number2}')
