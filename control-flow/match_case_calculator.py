0fba5d2d247fbec7c0ae8fdc59c9f00236ced7c6
awooda007 <frankstyles9@gmail.com>
2024-11-18 16:37:45 +0000

    Display the result of the operation in a user-friendly message

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,16 +1,18 @@
 num1 = input("Enter the first number:")
 num2 = input("Enter the second number:")
 operation = input("Choose the operation (+, -, *, /):")
-if operation == "+":
-	print(f"The result is {num1+num2}")
-elif operation == "-":
-	print(f"The result is {num1-num2}")	
-elif operation == "*":
-	print(f"The result is {num1*num2}")
-elif operation == "/":
-	if num2 == 0:
-		print("second number cannot be zero")
-	else:
-		print(f"The result is {num1/num2}")
-else:
-	print("invalid operation")
+
+match operation:
+	case "+":
+		print(f"The result is {num1+num2}")
+	case "-":
+		print(f"The result is {num1-num2}")	
+	case "*":
+		print(f"The result is {num1*num2}")
+	case "/":
+		if num2 == 0:
+			print("cannot divide by zero")
+		else:
+			print(f"The result is {num1/num2}")
+	case _:
+		print("invalid operation")
